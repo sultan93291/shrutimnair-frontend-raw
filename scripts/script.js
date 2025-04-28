ac964835c045a86308a062d72ea9bfe54ece5bd2
sultan93291 <abibdipto@gmail.com>
2025-04-28 15:36:48 +0600

    first two page done

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+const menuBtn = document.getElementById("menu-btn");
+const sidebar = document.getElementById("sidebar");
+const overlay = document.getElementById("overlay");
+const closeBtn = document.getElementById("close-btn");
+
+menuBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("-translate-x-full");
+  overlay.classList.toggle("opacity-0");
+  overlay.classList.toggle("pointer-events-none");
+  document.body.classList.toggle("overflow-hidden");
+});
+
+overlay.addEventListener("click", () => {
+  sidebar.classList.add("-translate-x-full");
+  overlay.classList.add("opacity-0", "pointer-events-none");
+  document.body.classList.remove("overflow-hidden");
+});
+
+closeBtn.addEventListener("click", () => {
+  sidebar.classList.add("-translate-x-full");
+  overlay.classList.add("opacity-0", "pointer-events-none");
+  document.body.classList.remove("overflow-hidden");
+});
